9d376e52b9ea6f898e9597ff5e0a795a3d331978
nishacodes <nishabatra27@gmail.com>
2013-02-20 21:01:33 -0500

    added association between pin and user

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,8 @@
-class Pin < ActiveRecord::Base
+	class Pin < ActiveRecord::Base
   attr_accessible :description
 
   validates :description, presence: true
+
+  belongs_to :user
+  validates :user_id, presence: true
 end
